6d8df9f8cbf47e1bede33d7f83694d95d60d9e2b
ukadike <ukadike.tochi@gmail.com>
2025-04-21 10:43:40 +0100

    Your changes

--- azure_project/deployment.py
+++ azure_project/deployment.py
@@ -3,9 +3,9 @@ from .settings import *
 from .settings import BASE_DIR
 
 
-SECRET_KEY = os.environ['SECRET']
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
-CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+SECRET_KEY = 'django-insecure-2#p9z0-d=39&qbu#&f1p6m=2+j!a4_n_r9q%wz192hfwva&24^'
+ALLOWED_HOSTS = "https://my-django-app-69ad5c.scm.azurewebsites.net/"
+CSRF_TRUSTED_ORIGINS = "https://my-django-app-69ad5c.scm.azurewebsites.net/detectors"
 DEBUG = False
 
 # WhiteNoise configuration
@@ -23,18 +23,6 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
-'''conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': conn_str_params['dbname'],
-        'HOST': conn_str_params['host'],
-        'USER': conn_str_params['user'],
-        'PASSWORD': conn_str_params['password'],
-    }
-}'''
 
 DATABASES = {
     'default': {
