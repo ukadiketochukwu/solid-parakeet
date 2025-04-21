5e5f8ae0ad615b13c71878f8856407c7a2e5bc4f
ukadike <ukadike.tochi@gmail.com>
2025-04-21 11:34:09 +0100

    okay maybe

--- azure_project/settings.py
+++ azure_project/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-2#p9z0-d=39&qbu#&f1p6m=2+j!a4_n_r9q%wz192hfwva&24^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["http://my-django-app-69ad5c.azurewebsites.net/"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
